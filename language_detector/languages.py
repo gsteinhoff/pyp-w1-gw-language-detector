911049c1770e88df6da05b6a296e601efccc3cd1
Gregory Steinhoff <gregory.steinhoff@organicvalley.coop>
2017-03-24 01:37:41 +0000

    Added final commit

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -30,7 +30,7 @@ LANGUAGES = [
     # HINT: https://en.wikipedia.org/wiki/Most_common_words_in_English
     {
         'name': 'English',
-        'common_words: [
+        'common_words': [
             'the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have',
             'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you',
             'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they',
@@ -43,5 +43,6 @@ LANGUAGES = [
             'come', 'its', 'over', 'think', 'also', 'back', 'after', 'use',
             'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new',
             'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us'
+        ]
     }
 ]
--- language_detector/main.py
+++ language_detector/main.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from languages import LANGUAGES
+
 """This is the entry point of the program."""
 
 def detect_language(text, languages):
     """Returns the detected language of given text."""
     # implement your solution here
-    pass
+    words = text.split()
+    count_matches = 0
+    output = ''
+    for lang in languages:
+        
+        matches = [word for word in words if word.lower() in lang['common_words']]
+        if (len(matches) > count_matches):
+            output = lang['name']
+            count_matches = len(matches)
+    return output
+    
+    
